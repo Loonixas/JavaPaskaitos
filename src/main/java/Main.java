357894cd981152ca3fa7e298b254f04d52d51a7f
Loonixas <loonixas@gmail.com>
2018-11-02 20:56:30 +0200

    su laukeliu ivesti zodi ir mygtuku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,34 @@
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.format.TextStyle;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Spark.port(8123);
+        Spark.get("/home", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                URI path=Main.class.getClassLoader().getResource("home.html").toURI();
+                String html=new String(Files.readAllBytes(Paths.get(path)), Charset.forName("UTF-8"));
+                return html;
+            }
+        });
+        Spark.post("/post", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                String param=request.queryParams("vardas");
+                return "Hello: "+param;
+            }
+        });
     }
 }
